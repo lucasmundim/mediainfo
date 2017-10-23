4ed81a36cab9086c32be62b92a7d44311d853d0a
Lucas Mundim <lucas.mundim@corp.globo.com>
2017-10-23 19:46:07 -0200

    Read from buffer

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 run:
-	go run mediainfo.go --init=dash/46_video_1_0_init.mp4 --input=dash/46_video_1_0_686.mp4
+	GODEBUG=cgocheck=0 go run mediainfo.go --init fixtures/sample_video_1_init.mp4 --input=fixtures/sample_video_1_1.mp4
--- mediainfo.go
+++ mediainfo.go
@@ -3,26 +3,52 @@
 // Tested with
 //
 // $ go run examples/mediainfo/medianfo.go --input=https://bintray.com/imkira/go-libav/download_file?file_path=sample_iPod.m4v
+// $ GODEBUG=cgocheck=0 go run mediainfo.go --init fixtures/sample_video_1_init.mp4 --input=fixtures/sample_video_1_1.mp4
 //
 // stream 0: eng aac audio, 2 channels, 44100 Hz
 // stream 1: h264 video, 320x240
 
 package main
 
+//#include <libavutil/avutil.h>
+//#include <libavutil/avstring.h>
+//#include <libavcodec/avcodec.h>
+//#include <libavformat/avformat.h>
+//#include <libavformat/avio.h>
+//
+// typedef struct buffer_data {
+//     uint8_t *ptr;
+//     size_t size; ///< size left in the buffer
+// } buffer_data;
+//
+// int read_packet(void *opaque, uint8_t *buf, int buf_size)
+// {
+//     struct buffer_data *bd = (struct buffer_data *)opaque;
+//     buf_size = FFMIN(buf_size, bd->size);
+//     printf("ptr:%p size:%zu\n", bd->ptr, bd->size);
+//     /* copy internal buffer data to buf */
+//     memcpy(buf, bd->ptr, buf_size);
+//     bd->ptr  += buf_size;
+//     bd->size -= buf_size;
+//     return buf_size;
+// }
+//
+// #cgo pkg-config: libavformat libavutil
+import "C"
+
 import (
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
-	"os"
+	"unsafe"
 
 	"github.com/imkira/go-libav/avcodec"
 	"github.com/imkira/go-libav/avformat"
 	"github.com/imkira/go-libav/avutil"
 )
 
-var initFileName string
-var inputFileName string
+var initFileName, inputFileName string
 
 func init() {
 	flag.StringVar(&initFileName, "init", "", "init file to probe")
@@ -39,25 +65,18 @@ func main() {
 		log.Fatalf("Missing --input=file\n")
 	}
 
-	tempFile, err := ioutil.TempFile(os.TempDir(), "prefix")
-	defer os.Remove(tempFile.Name())
-
-	var data []byte
+	var buffer []byte
 	initData, err := ioutil.ReadFile(initFileName)
 	if err != nil {
 		panic(err)
 	}
-	data = append(data, initData...)
+	buffer = append(buffer, initData...)
 
 	inputData, err := ioutil.ReadFile(inputFileName)
 	if err != nil {
 		panic(err)
 	}
-	data = append(data, inputData...)
-
-	if _, err = tempFile.Write(data); err != nil {
-		panic(err)
-	}
+	buffer = append(buffer, inputData...)
 
 	// open format (container) context
 	decFmt, err := avformat.NewContextForInput()
@@ -69,8 +88,24 @@ func main() {
 	options := avutil.NewDictionary()
 	defer options.Free()
 
+	var bufferSize C.int
+	bufferSize = C.int(8192)
+	readBufferSize := C.size_t(bufferSize)
+	readExchangeArea := C.av_malloc(readBufferSize)
+	// defer C.av_free(unsafe.Pointer(readExchangeArea))
+
+	var bd C.buffer_data
+	bd.ptr = (*C.uint8_t)(unsafe.Pointer(&buffer[0]))
+	bd.size = C.size_t(bufferSize)
+
+	cCtx := C.avio_alloc_context((*C.uchar)(readExchangeArea), bufferSize, 0, unsafe.Pointer(&bd), (*[0]byte)(C.read_packet), nil, nil)
+	defer C.av_free(unsafe.Pointer(cCtx))
+	ioCtx := avformat.NewIOContextFromC(unsafe.Pointer(cCtx))
+	// defer C.av_free(unsafe.Pointer(ioCtx))
+	decFmt.SetIOContext(ioCtx)
+
 	// open file for decoding
-	if err := decFmt.OpenInput(tempFile.Name(), nil, options); err != nil {
+	if err := decFmt.OpenInput("", nil, options); err != nil {
 		log.Fatalf("Failed to open input file: %v", err)
 	}
 	defer decFmt.CloseInput()
